6e74efecf82037a033093afc499b9958acbe4670
ushasri2645 <u.gudikandula@everest.engineering>
2024-08-01 13:06:39 +0530

    Inserting line by line

--- Data/ReadFiles.ts
+++ Data/ReadFiles.ts
@@ -1,5 +1,7 @@
 import * as fs from 'fs';
+import { parse } from 'csv-parse';
 import csv from 'csv-parser';
+import readline from 'readline'
 import nconf from "../Config/fileconfig";
 import { movieModel } from '../Models/movie';
 import {userReviewModel} from '../Models/user'
@@ -14,16 +16,6 @@ export const readFiles = () => {
     return {movieFile,userFile,criticFile};
 }
 
-// export const readCSVFile  = async <T>(filepath:string,name=''): Promise<T[]> => { 
-//     return new Promise((resolve,reject) => { 
-//         let results: T[] = [];  
-//         const stream = fs.createReadStream(filepath)  
-//         stream.pipe(csv()).on('data',(data)=>results.push(data))   
-//                           .on('end',()=>resolve(results)) 
-//                           .on('error',(error:string)=>reject(error)) 
-//          });
-// }
-
 const change = async(data: any) =>{
     const movie = await movieModel.findOne({movieId: data.movieId},{_id:1});
      if(movie){
@@ -31,63 +23,49 @@ const change = async(data: any) =>{
         return data!;
     }
 }
-// export const readCsvFile  = async <T>(filepath:string, name='m'): Promise<T[]> => {
-//     return new Promise((resolve,reject) => {
-//         const results:T[] = [];
-//         const stream = fs.createReadStream(filepath)
-//         const csvStream = csv()
-//         stream.pipe(csvStream).on('data',async (data:any)=>{
-//                             if(name==='m'){ 
-//                                 await movieModel.create(data)
-//                             }
-//                             else if(name==='u'){
-//                                 const item = await change(data);
-//                                 await userReviewModel.create(item)
-//                             }
-//                             else{
-//                                 const item = await change(data);
-//                                 await criticModel.create(item)
-//                             }
-//                         })
-//                           .on('end',()=>{
-//                             console.log(`Data inserted succesfully into ${name} Model`);
-//                             resolve(results)})
-//                           .on('error',(error:string)=>reject(error))
-//          })       
-// }
 
-export const readCsvFile  = async(filepath:string, name='m') => {
-    return new Promise((resolve,reject) => {
+
+export const readCsvFile = async (filepath: string, name = 'm') => {
+    return new Promise((resolve, reject) => {
+        console.log(`Processing the file ${filepath}`)
         const stream = fs.createReadStream(filepath)
         const csvStream = csv();
         let processing = false;
-        stream.pipe(csvStream).on('data',async (data:any)=>{
-                            stream.pause();
-                            try{
-                                processing=true;
-                                if(name==='m'){ 
-                                    await movieModel.create(data)
-                                }
-                                else if(name==='u'){
-                                    const item = await change(data);
-                                    await userReviewModel.create(item)
-                                }
-                                else{
-                                    const item = await change(data);
-                                    await criticModel.create(item)
-                                }
-                            }
-                            catch(e){
-                                console.log("Error:",e);
-                            }
-                            finally {
-                                processing = false;
-                                stream.resume();
-                            }
-                        })
-                          .on('end',()=>{
-                            // console.log(`Data inserted succesfully into ${name} Model`);
-                            resolve(`Data inserted succesfully into ${name} Model`)})
-                          .on('error',(error:string)=>reject(error))
-         })       
+        let recordCount = 0
+        const csvPipe = stream.pipe(csvStream)
+        csvPipe.on('data', async (data: any) => {
+            console.log(`Processing Record ${++recordCount} `, data)
+            csvPipe.pause();
+            try {
+                processing = true;
+                if (name === 'm') {
+                    let movieObject = new movieModel(data)
+                    await movieObject.save()
+                    console.log(`Done with Saving Record ${recordCount} `, data)
+                }
+                else if (name === 'u') {
+                    const item = await change(data);
+                    await userReviewModel.create(item)
+                }
+                else {
+                    const item = await change(data);
+                    await criticModel.create(item)
+                }
+            }
+            catch (e) {
+                console.log("Error:", e);
+            }
+            finally {
+                csvPipe.resume();
+                processing = false;
+            }
+        })
+            .on('end', () => {
+                console.log(`EOF file reached for ${name} Model`);
+                // resolve(`Data inserted succesfully into ${name} Model`)
+            })
+            .on('error', (error: string) => reject(error))
+    })
 }
+
+
